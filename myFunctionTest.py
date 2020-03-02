d25bd3d3b8f95376785cf552addd53d334f46354
cowen2002 <cowen2002@qq.com>
2020-03-03 01:21:51 +0800

    modified myFunctionTest.py

--- myFunctionTest.py
+++ myFunctionTest.py
@@ -16,10 +16,23 @@ def func1(**kwargs):   #as a dict
     for key, value in kwargs.items():
         print(key,'=',value)
         
+        
+def func2():
+    print("还款期数    还款本金    还款手续费(利息)    剩余本金")
+    for i in range(60):        
+        print('第%2d期' % int(i+1), end='       ')
+        print('1667', end='            ')
+        print('225', end='           ')
+        print('%d' % int(100000-1667*(i+1)))
+        
+        
+        
+        
 
 
 
 
 if __name__ == '__main__':
-    print(avg(1,2,3,4,5,6))
-    func1(key1='hello', key2='world', key3=3)
+    #print(avg(1,2,3,4,5,6))
+    #func1(key1='hello', key2='world', key3=3)
+    func2()
